4c5586ecde8ffbbb1f924eed8781eba3a9e05d52
Rui Lebre <rui.andre.lebre@gmail.com>
2024-03-12 10:20:11 +0000

    Fixed tests

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MailService } from './mail.service';
+import { MailerService } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
 
 describe('MailService', () => {
   let service: MailService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MailService]
+      providers: [
+        MailService,
+        {
+          provide: MailerService,
+          useValue: {
+            sendMail: jest.fn()
+          }
+        },
+        ConfigService
+      ]
     }).compile();
 
     service = module.get<MailService>(MailService);
